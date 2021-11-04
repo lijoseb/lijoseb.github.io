6e1ee793d141596cd340795a682993422d318d52
sebastian <89874669+lijoseb@users.noreply.github.com>
2021-11-04 14:45:51 -0400

    Delete p2 2.html directory

--- p2 2.html/assets/js/layout.js
+++
@@ -1,9 +0,0 @@
-$(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 800) {
-        $('.title direct').fadeIn();
-    } else {
-        $('.title direct').fadeOut();
-    }
-
-});
